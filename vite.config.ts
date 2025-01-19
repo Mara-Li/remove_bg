701fe92aa55a9b750ad2deeaaede213a61ee9a05
Mara <mara-li@outlook.fr>
2025-01-20 00:00:11 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,29 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "",
 	plugins: [react()],
+	root: "src",
+	publicDir: "../public",
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "index.html"), // Point d'entrée principal
-				remove_bg: resolve(__dirname, "src", "remove_bg/index.html"), // Point d'entrée secondaire
+				main: "./src/index.html", // Point d'entrée principal
+				remove_bg: "./src/remove_bg/index.html", // Point d'entrée secondaire
 			},
 		},
+		assetsDir: "assets",
+		outDir: "../dist", // Dossier de sortie
+		emptyOutDir: true, // Vide le dossier de sortie avant de construire
 	},
 	server: {
 		fs: {
 			allow: ["."], // Permet d'accéder au sous-projet remove_bg
+		},
+	},
+	resolve: {
+		alias: {
+			remove_bg: resolve(__dirname, "remove_bg"), // Alias pour le sous-projet remove_bg
 		},
 	},
 });
