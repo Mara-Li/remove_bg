d836e2827bfc366fc6706b4d63970e4fd752cdb3
Mara <mara-li@outlook.fr>
2025-01-20 10:49:28 +0100

    fix: metadata generation at build

--- src/Meta.tsx
+++ src/Meta.tsx
@@ -12,14 +12,17 @@ export default function ({ project }: { project: string }) {
 			<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 			<title>{meta.title}</title>
 			<meta name="description" content={meta.description} />
+			{/* Open Graph */}
 			<meta property="og:title" content={meta.title} />
 			<meta property="og:description" content={meta.description} />
 			<meta property="og:image" content={meta.og_image} />
 			<meta property="og:url" content={meta.url} />
+			{/* Twitter */}
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:title" content={meta.title} />
 			<meta name="twitter:description" content={meta.description} />
 			<meta name="twitter:image" content={meta.og_image} />
+			{/* Icons */}
 			<link href={meta.icon} rel="icon" type="image/x-icon" />
 			<link href={meta.icon} rel="apple-touch-icon" />
 		</Helmet>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,67 @@
-import { resolve } from "node:path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import react from "@vitejs/plugin-react";
+import { JSDOM } from "jsdom";
 import { defineConfig } from "vite";
 
+function extractMetadataPlugin() {
+	return {
+		name: "vite-plugin-extract-metadata",
+		buildStart() {
+			console.log("Extraction des métadonnées...");
+
+			const projects = ["./remove_bg", "."]; // Dossiers contenant les fichiers index.html
+			const metadata = [];
+
+			for (const project of projects) {
+				const filePath = path.resolve(
+					__dirname,
+					"src",
+					`${project}/index.html`,
+				);
+				console.log(filePath);
+				if (fs.existsSync(filePath)) {
+					const htmlContent = fs.readFileSync(filePath, "utf-8");
+
+					// Fonction pour extraire les <meta> tags
+					const dom = new JSDOM(htmlContent);
+					const document = dom.window.document;
+
+					// Fonction pour extraire une balise meta par propriété
+					const extractMeta = (property: string): string | undefined => {
+						const meta = document.querySelector(`meta[property="${property}"]`);
+						return meta?.getAttribute("content") || undefined;
+					};
+
+					const title = extractMeta("og:title");
+					const description = extractMeta("og:description");
+					const image = extractMeta("og:image");
+					const url = extractMeta("og:url");
+					const icon = extractMeta("icon");
+					const projectName = `/${project.replaceAll("/", "").replaceAll(".", "")}/`;
+					metadata.push({
+						project: projectName.replaceAll("//", "/"),
+						title,
+						description,
+						url,
+						og_image: image,
+						icon,
+					});
+				} else {
+					console.warn(`Fichier ${filePath} introuvable. Ignoré.`);
+				}
+			}
+
+			// Sauvegarder les métadonnées dans un fichier JSON
+			const metadataFile = path.resolve(__dirname, "./src/metadata.json");
+			fs.writeFileSync(metadataFile, JSON.stringify(metadata, null, 2));
+			console.log("Métadonnées extraites avec succès !");
+		},
+	};
+}
+
 export default defineConfig({
-	base: "",
-	plugins: [react()],
+	plugins: [react(), extractMetadataPlugin()],
 	root: "src",
 	publicDir: "../public",
 	build: {
@@ -21,11 +78,6 @@ export default defineConfig({
 	server: {
 		fs: {
 			allow: ["."], // Permet d'accéder au sous-projet remove_bg
-		},
-	},
-	resolve: {
-		alias: {
-			remove_bg: resolve(__dirname, "remove_bg"), // Alias pour le sous-projet remove_bg
 		},
 	},
 });
